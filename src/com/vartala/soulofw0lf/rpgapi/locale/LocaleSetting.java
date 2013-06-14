e3670d0fc09bfe5db5d5b7203b2ce7300376b1b8
soulofwolf <web_wizardz@mail.com>
2013-06-14 16:42:32 -0500

    - updated to include remote entities / removed worldguard

---
+++ src/com/vartala/soulofw0lf/rpgapi/locale/LocaleSetting.java
@@ -0,0 +1,11 @@
+package com.vartala.soulofw0lf.rpgapi.locale;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: soulof
+ * Date: 6/13/13
+ * Time: 10:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocaleSetting {
+}
