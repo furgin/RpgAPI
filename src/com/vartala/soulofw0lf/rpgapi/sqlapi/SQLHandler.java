d8c40c28d7c91dd41216029dc86ce6adb8bb041f
Tennyson Holloway <tennyson.holloway@gmail.com>
2013-06-24 13:41:09 -0700

    sql handling in place

--- src/com/vartala/soulofw0lf/rpgapi/sqlapi/SQLHandler.java
+++ src/com/vartala/soulofw0lf/rpgapi/sqlapi/SQLHandler.java
@@ -4,35 +4,110 @@ import com.vartala.soulofw0lf.rpgapi.RpgAPI;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-public class SQLHandler {
-    public static String user = "", password = "", url = "";
-    public static Properties properties;
-
-    /* initialize the static sqlhandler and properties */
-    public SQLHandler(String username, String passwordd, String urll) {
-        user = username;
-        password = passwordd;
-        url = "jdbc:mysql://" + url;
-        properties = new Properties();
-        properties.put("user", RpgAPI.dBUserName);
-        properties.put("password", RpgAPI.dBPassword);
-        properties.put("characterEncoding", RpgAPI.dBEncoding);
-        properties.put("useUnicode", RpgAPI.uniCode);
+public class SQLHandler extends SQLOps {
+    private String host, database, username, password;
+    private Connection connection;
+    private static final Logger SQLLogger = Logger.getLogger("SQLHandler");
+    /**
+     * Used for proper logging
+     */
+    public static void logSevere(String severe)
+    {
+        SQLLogger.severe(severe);
+    }
+    /**
+     * Reopens the SQL connection if it is closed. This is invoked upon every
+     * query.
+     */
+    public void refreshConnection() {
+        if (connection == null) {
+            initialise();
+        }
     }
 
-    /* url should be as follows tennysonholloway.com not http://www.tennysonholloway.com */
-    public static Connection openConnection() {
+    /**
+     * Manually close the connection.
+     */
+    public void closeConnection() {
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection c = DriverManager.getConnection(url, properties);
-            return c;
-        } catch (Exception e) {
+            this.connection.close();
+        } catch (SQLException e) {
             e.printStackTrace();
-            RpgAPI.logger.severe("Unable to open the sql connection...make sure your settings are correct");
         }
-        return null;
+    }
+
+    /**
+     * Initialise a new connection using RPGApi settings
+     *
+     * @return
+     */
+    public boolean initialise() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + RpgAPI.dBURL, RpgAPI.dBUserName, RpgAPI.dBPassword);
+            return true;
+        } catch (ClassNotFoundException ex) {
+            logSevere("Could not find MySQL driver class!");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            logSevere("SQL Exception!");
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Any query which does not return a ResultSet object. Such as : INSERT,
+     * UPDATE, CREATE TABLE...
+     *
+     * @param query
+     */
+    public void standardQuery(String query) throws SQLException {
+        this.refreshConnection();
+        super.standardQuery(query, this.connection);
+    }
+
+    /**
+     * Check whether a field/entry exists in a database.
+     *
+     * @param query
+     * @return Whether or not a result has been found in the query.
+     * @throws SQLException
+     */
+    public boolean existanceQuery(String query) throws SQLException {
+        this.refreshConnection();
+        return super.sqlQuery(query, this.connection).next();
+    }
+
+    /**
+     * Any query which returns a ResultSet object. Such as : SELECT Remember to
+     * close the ResultSet object after you are done with it to free up
+     * resources immediately. ----- ResultSet set =
+     * sqlQuery("SELECT * FROM sometable;"); set.doSomething(); set.close();
+     * -----
+     *
+     * @param query
+     * @return ResultSet
+     */
+    public ResultSet sqlQuery(String query) throws SQLException {
+        this.refreshConnection();
+        return super.sqlQuery(query, this.connection);
+    }
+
+    /**
+     * Check whether the table name exists.
+     *
+     * @param table
+     * @return
+     */
+    public boolean doesTableExist(String table) throws SQLException {
+        this.refreshConnection();
+        return super.checkTable(table, this.connection);
     }
 
 }
---
+++ src/com/vartala/soulofw0lf/rpgapi/sqlapi/SQLOps.java
@@ -0,0 +1,46 @@
+package com.vartala.soulofw0lf.rpgapi.sqlapi;
+
+import java.sql.*;
+
+/**
+ * Created by: tennyson.holloway
+ * Date: 6/24/13
+ * Time: 1:25 PM
+ * <p/>
+ * This file is part of the Rpg Suite Created by Soulofw0lf and Linksy.
+ * <p/>
+ * The Rpg Suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * The Rpg Suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with The Rpg Suite Plugin you have downloaded.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class SQLOps {
+    protected synchronized int standardQuery(String query, Connection connection) throws SQLException{
+        Statement statement = connection.createStatement();
+        int rowsUpdated = statement.executeUpdate(query);
+        statement.close();
+        return rowsUpdated;
+    }
+
+    protected synchronized ResultSet sqlQuery(String query, Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
+        return result;
+    }
+
+    protected synchronized boolean checkTable(String table, Connection connection) throws SQLException {
+        DatabaseMetaData dbm;
+        dbm = connection.getMetaData();
+        ResultSet tables = dbm.getTables(null, null, table, null);
+        return tables.next();
+    }
+
+}
